a2085cd53c9ad4ee9082eb55fad7a546567b858a
dfx81 <danialfitrighazali@gmail.com>
2022-05-22 15:13:24 +0800

    Change some routes

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    let link = document.querySelector("#close");
-
-    link.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        window.close();
-    });
-}
